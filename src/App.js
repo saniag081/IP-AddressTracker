109ee7733465651c2ab2758708ff621f237cc52f
saniag081 <sgonzalezhernandez081@gmail.com>
2021-01-07 21:24:22 -0500

    fix hook useRequest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Map from './component/Map/Map'
 import Header from './component/Header'
+import useRequest from './hooks/useRequest'
 
 function App() {
-  // const position = [51.505, -0.09]
-
+  const response = useRequest()
+  console.log(response)
   return (
     <div className="App">
       <Header />
--- src/hooks/useRequest/index.js
+++ src/hooks/useRequest/index.js
@@ -1,10 +1,12 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useRequest(ip='8.8.8.8') {
+	const [getDataResponse, setDataResponse] = useState()
 	const url = `https://geo.ipify.org/api/v1?apiKey=at_WXluPfPH0crN2rZeG2AKjpMtLJroT&ipAddress=${ip}`
 	useEffect(() => {
 		fetch(url)
 			.then(response => response.json())
-			.then(data => console.log(data))
-	})
+			.then(data => setDataResponse(data))
+	},[url])
+	return getDataResponse
 }
