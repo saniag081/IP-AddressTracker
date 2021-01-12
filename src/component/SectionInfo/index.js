c0e458867cb381bded03eca3c95f56ccd36d0255
saniag081 <sgonzalezhernandez081@gmail.com>
2021-01-11 22:01:58 -0500

    added  data component SectionInfo

--- src/component/SectionInfo/index.js
+++ src/component/SectionInfo/index.js
@@ -1,26 +1,38 @@
 import './SectionInfo.css'
+import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 export default function SectionInfo() {
+	const [getData, setData] = useState({})
+	const { dataApi } = useSelector(data => data)
+	console.log(getData)
+	useEffect(() => {
+		setData(dataApi)
+	},[dataApi])
 	return (
-		<div className='c-info'>
-			<div className='c-info-container'>
-				<div className='c-info-container-box'>
-					<span className='u-color-gray'>IP Adress</span>
-					<h3>192.212.174.101</h3>
-				</div>
-				<div className='c-info-container-box'>
-					<span className='u-color-gray'>LOCATION</span>
-					<h3>Brooklyn, NY</h3>
-				</div>
-				<div className='c-info-container-box'>
-					<span className='u-color-gray'>TIMEZONE</span>
-					<h3>UTC - 05:00</h3>
-				</div>
-				<div className='c-info-container-box'>
-					<span className='u-color-gray'>ISP</span>
-					<h3>Space X</h3>
+	<>
+		{getData.loading ? 'loading'
+		:	<div className='c-info'>
+				<div className='c-info-container'>
+					<div className='c-info-container-box'>
+						<span className='u-color-gray'>IP Adress</span>
+						<h3>{ getData.getDataResponse?.ip }</h3>
+					</div>
+					<div className='c-info-container-box'>
+						<span className='u-color-gray'>LOCATION</span>
+							<h3>{getData.getDataResponse?.location.city}</h3>
+					</div>
+					<div className='c-info-container-box'>
+						<span className='u-color-gray'>TIMEZONE</span>
+							<h3>{ getData.getDataResponse?.location.timezone }</h3>
+					</div>
+					<div className='c-info-container-box'>
+						<span className='u-color-gray'>ISP</span>
+							<h3>{  getData.getDataResponse?.isp}</h3>
+					</div>
 				</div>
 			</div>
-			</div>
+			}
+		</>
 	)
 }
