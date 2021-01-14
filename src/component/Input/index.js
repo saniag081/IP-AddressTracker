98fb783a040443bee41b99726a17b404bd4e5636
saniag081 <sgonzalezhernandez081@gmail.com>
2021-01-13 20:57:23 -0500

    added event form

--- src/component/Input/index.js
+++ src/component/Input/index.js
@@ -1,12 +1,33 @@
 import './Input.css'
+import { useRef } from 'react'
 
 export default function Input() {
+	const refForm = useRef()
+	function handleSubmit(e) {
+		e.preventDefault()
+		const elem = refForm.current
+		const data = new FormData(elem)
+		console.log(data.get('text_ip'))
+	}
 	return (
-		<label  className='c-header-input'>
-			<input id="inputIp" className='c-header-input-elem' type='text' placeholder='Search for any IP address or domain' />
-			<span className="material-icons">
-				keyboard_arrow_right
-			</span>
-		</label>
+		<form
+			method="post"
+			onSubmit={handleSubmit}
+			ref={refForm}
+		>
+			<label  className='c-header-input'>
+				<input
+					id="inputIp"
+					name="text_ip"
+					className='c-header-input-elem'
+					type='number'
+					required
+					placeholder='Search for any IP address or domain'
+				/>
+				<span className="material-icons">
+					keyboard_arrow_right
+				</span>
+			</label>
+		</form>
 	)
 }
